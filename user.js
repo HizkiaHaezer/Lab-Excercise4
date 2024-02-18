aa3875cc656f4df8dd9b0c9efbdc9d171d9bb1e7
Hizkia Haezer <145329254+HizkiaHaezer@users.noreply.github.com>
2024-02-18 13:13:06 +0800

    first init github

---
+++ user.js
@@ -0,0 +1,16 @@
+users = [
+    {
+        id: 1,
+        name: "Mark",
+    },
+    {
+        id: 2,
+        name: "Finly",
+    },
+    {
+        id: 3,
+        name: "Smith",
+    },
+];
+
+module.exports = users;
